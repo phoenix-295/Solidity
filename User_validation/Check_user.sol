ae69c5a5af9f48f7ea816cb9f81c5d6384927889
phoenix-295 <nikhil295@gmail.com>
2022-04-15 13:11:44 +0530

    first commit

---
+++ User_validation/Check_user.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Storage {
+    
+    function check_pass(string memory usr, string memory pass) public pure returns (string memory){
+        if ((keccak256(bytes(usr)) == keccak256(bytes("Nik"))) && keccak256(bytes(pass)) == keccak256(bytes("123"))){
+            // return true;
+            return "Valid user";
+        } else {
+            return "Invaid user";
+        }
+    }
+}
